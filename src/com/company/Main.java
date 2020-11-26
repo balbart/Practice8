018c260ab515878300716d49bd514d263b1f5959
balbart <balbart8@gmail.com>
2020-11-26 13:45:45 +0800

    Допилил для вменяемого вида

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,40 +6,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        PhoneBook phoneBook = new PhoneBook();
-        breakPoint:
-        while (true) {
-            String something = in.nextLine();
-            if(something.matches("LIST")){
-                phoneBook.printBook();
-            }
-            else if(something.matches("EXIT")){
-                break breakPoint;
-            }
-            else if(PhoneBook.isName(something)){
-                String mayNumber = in.nextLine();
-                if(PhoneBook.isPhoneNumber(mayNumber)){
-                    phoneBook.addContact(something, mayNumber);
-                }
-                else{
-                    System.out.println("Неправильный ввод");
-                }
-            }
-            else if(PhoneBook.isPhoneNumber(something)){
-                String mayName = in.nextLine();
-                if(PhoneBook.isName(mayName)){
-                    phoneBook.addContact(mayName, something);
-                }
-                else{
-                    System.out.println("Неправильный ввод");
-                }
-            }
-
-            else{
-                System.out.println("Неправильный ввод");
-            }
-        }
+        //Main'ы каждого из заданий лежат под самими классами
     }
 }
 
--- src/com/company/PhoneBook.java
+++ src/com/company/PhoneBook.java
@@ -110,3 +110,42 @@ public class PhoneBook {
         this.phoneBook = new TreeMap<>(this.phoneBook);
     }
 }
+
+class PhoneBookLoader{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        PhoneBook phoneBook = new PhoneBook();
+        breakPoint:
+        while (true) {
+            String something = in.nextLine();
+            if(something.matches("[Ll][Ii][Ss][Tt]")){
+                phoneBook.printBook();
+            }
+            else if(something.matches("EXIT")){
+                break breakPoint;
+            }
+            else if(PhoneBook.isName(something)){
+                String mayNumber = in.nextLine();
+                if(PhoneBook.isPhoneNumber(mayNumber)){
+                    phoneBook.addContact(something, mayNumber);
+                }
+                else{
+                    System.out.println("Неправильный ввод");
+                }
+            }
+            else if(PhoneBook.isPhoneNumber(something)){
+                String mayName = in.nextLine();
+                if(PhoneBook.isName(mayName)){
+                    phoneBook.addContact(mayName, something);
+                }
+                else{
+                    System.out.println("Неправильный ввод");
+                }
+            }
+
+            else{
+                System.out.println("Неправильный ввод");
+            }
+        }
+    }
+}
